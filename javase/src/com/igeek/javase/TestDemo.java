4150b84dc8d1ec07bc557da0c174f3dd07927664
bcc123456789 <bcc1725863654@outlook.com>
2021-01-22 16:54:14 +0800

    bcc submit

---
+++ javase/src/com/igeek/javase/TestDemo.java
@@ -0,0 +1,14 @@
+package com.igeek.javase;
+
+/**
+ * @author ：Allen
+ * @date ：Created in 2021/1/22 16:51
+ * @description：
+ * @version: 1.0
+ */
+public class TestDemo {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
